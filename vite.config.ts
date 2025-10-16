d0dc6cb922101655e5ad78cf6ab88acadab762b0
karan-uppal3 <57282321+karan-uppal3@users.noreply.github.com>
2025-10-16 18:40:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: 'karan-uppal3.github.io',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
